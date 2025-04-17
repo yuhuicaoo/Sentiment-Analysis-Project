2a1f4f686e37ebd14df8e063b804bdc0da5218ae
Yuhui <yuhuicaowork@gmail.com>
2025-04-18 00:29:23 +1200

    Update train.py

--- train.py
+++ train.py
@@ -1,10 +1,11 @@
 import torch 
 import torch.nn as nn
+import os
 
 def train_one_epoch(train_loader, optimiser, model, loss_fn, device):
     running_loss = 0
 
-    for i, data in enumerate(train_loader):
+    for _, data in enumerate(train_loader):
         inputs, atn_mask, labels = data['input_ids'], data['attention_mask'], data['label']
         inputs, atn_mask, labels = inputs.to(device), atn_mask.to(device), labels.to(device)
 
@@ -40,7 +41,7 @@ def train_model(train_loader, val_loader, optimiser, model, loss_fn, device, epo
         running_loss = 0
         # disable gradient computation and reduce memory consumption
         with torch.no_grad():
-            for i, vdata in enumerate(val_loader):
+            for _, vdata in enumerate(val_loader):
                 vinputs, vatn_mask, vlabels = vdata['input_ids'], vdata['attention_mask'], vdata['label']
                 vinputs, vatn_mask, vlabels = vinputs.to(device), vatn_mask.to(device), vlabels.to(device)
 
@@ -50,4 +51,14 @@ def train_model(train_loader, val_loader, optimiser, model, loss_fn, device, epo
         
         avg_vloss = running_loss / len(train_loader)
         print(f'LOSS train {avg_loss:.4f} validation {avg_vloss:.4f}')
+
+        # save model
+        model_dir = 'models'
+        if not os.path.exists(model_dir):
+            os.makedirs(model_dir)
+        
+        models_count = len(os.listdir(model_dir))
+
+        model_name = os.path.join(model_dir, f'model{models_count + 1}.pth')
+        torch.save(model.state_dict(), model_name)
                 
