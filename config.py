089a4c9da22b8f29496385526091a4968838d565
Yuhui <yuhuicaowork@gmail.com>
2025-04-18 14:22:58 +1200

    Update config.py

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import torch
 
 # hyperparamters - change values around for testing.
-batch_size = 64
+batch_size = 32
 block_size = 64
-learning_rate = 3e-4
+learning_rate = 1e-4
 num_embd = 96
 num_heads = 4
 num_layers = 3
-dropout = 0.4
+dropout = 0.5
 device = "cuda" if torch.cuda.is_available() else "cpu"
 # ---------------------
