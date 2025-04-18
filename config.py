12006b62f657f5f26a73627cc22b49dc4bd6ac85
Yuhui <yuhuicaowork@gmail.com>
2025-04-18 14:17:24 +1200

    Update config.py

--- config.py
+++ config.py
@@ -4,9 +4,9 @@ import torch
 batch_size = 64
 block_size = 64
 learning_rate = 3e-4
-num_embd = 128
+num_embd = 96
 num_heads = 4
-num_layers = 4
-dropout = 0.3
+num_layers = 3
+dropout = 0.4
 device = "cuda" if torch.cuda.is_available() else "cpu"
 # ---------------------
